30c00972d6984a1c6e8cb7a58d979d4a95e4509b
Endre Kollár <taxy443@gmail.com>
2013-03-26 11:05:33 +0100

    Rework peak-common

--- peak_common.py
+++ peak_common.py
@@ -6,7 +6,7 @@ import apt_pkg
 
 type_recommends = 4
 
-class RevdependsCounter:
+class Peak:
 
     def __init__(self, cache):
         self.cache = cache
@@ -30,54 +30,75 @@ class RevdependsCounter:
     def simulated_remove(self, pkg):
         self.removed.add(pkg.id)
 
-    def count_pkg_revdepends(self, pkg, maxcount):
-        self.maxcount = maxcount
-        self.pkg_except = set()
-        return self.count_pkg_revdepends_loop(pkg)
-
-    def count_pkg_revdepends_except(self, pkg, maxcount, pkg_except):
-        self.maxcount = maxcount
-        self.pkg_except = pkg_except
-        return self.count_pkg_revdepends_loop(pkg)
-
-    def count_pkg_revdepends_loop(self, pkg):
-
-        self.verboseprint("Revdeps:", pkg.get_fullname(True))
-        rev_depends = 0
+    def has_revdeps_without_provides(self, pkg):
+        self.verboseprint("Reverse depends:", pkg.get_fullname(True))
         for otherdep in pkg.rev_depends_list:
             if self.installed(otherdep.parent_pkg) and \
                     otherdep.parent_pkg.current_ver.id == otherdep.parent_ver.id and \
                     (otherdep.dep_type_enum == apt_pkg.Dependency.TYPE_DEPENDS or \
-                    otherdep.dep_type_enum == apt_pkg.Dependency.TYPE_PREDEPENDS) and \
-                    not otherdep.parent_pkg.id in self.pkg_except:
-                rev_depends += 1
-                if rev_depends >= self.maxcount:
-                    self.verboseprint("Found revdep:", otherdep.parent_pkg.get_fullname(True))
-                    return rev_depends
+                    otherdep.dep_type_enum == apt_pkg.Dependency.TYPE_PREDEPENDS):
+                self.verboseprint("Found reverse depend:", otherdep.parent_pkg.get_fullname(True))
+                return True
+        return False
+
+    def collect_provided_revdeps2(self, pkg):
+        for otherdep in pkg.rev_depends_list:
+            if self.installed(otherdep.parent_pkg) and \
+                    otherdep.parent_pkg.current_ver.id == otherdep.parent_ver.id and \
+                    (otherdep.dep_type_enum == apt_pkg.Dependency.TYPE_DEPENDS or \
+                    otherdep.dep_type_enum == apt_pkg.Dependency.TYPE_PREDEPENDS):
+                self.verboseprint("\t", otherdep.parent_pkg.get_fullname(True))
+                self.provided_revdeps.append(otherdep.parent_pkg)
+
+
+    def collect_provided_revdeps(self, pkg):
         if self.installed(pkg):
             for provided in pkg.current_ver.provides_list:
                 try:
                     provides_pkg = self.cache[provided[0]]
                     if not self.installed(provides_pkg):
                         self.verboseprint("Found provide:", provides_pkg.get_fullname(True))
-                        rev_depends += self.count_pkg_revdepends_loop(provides_pkg)
-                    if rev_depends >= self.maxcount:
-                            return rev_depends
+                        self.collect_provided_revdeps2(provides_pkg)
                 except KeyError as e:
-                    print("Package not found:", str(e).strip('"'))
+                    self.verboseprint("Package not found:", str(e).strip('"'))
 
-        return rev_depends
+    def collect_revrecommends(self, pkg):
+        global type_recommends
 
-class CirclelessRevdependsCounter:
+        self.verboseprint("Revrecommends:", pkg.get_fullname(True))
+        for otherdep in pkg.rev_depends_list:
+            if self.rev_c.installed(otherdep.parent_pkg) and \
+                    otherdep.parent_pkg.current_ver.id == otherdep.parent_ver.id and \
+                    otherdep.dep_type_enum == type_recommends:
+                self.verboseprint("\t", otherdep.parent_pkg.get_fullname(True))
+                self.revrecommends.append(otherdep.parent_pkg)
 
-    def __init__(self, rev_c):
-        self.rev_c = rev_c
+    def has_revdepends_loop(self, pkg):
+
+        for otherdep in pkg.rev_depends_list:
+            if self.installed(otherdep.parent_pkg) and \
+                    otherdep.parent_pkg.current_ver.id == otherdep.parent_ver.id and \
+                    (otherdep.dep_type_enum == apt_pkg.Dependency.TYPE_DEPENDS or \
+                    otherdep.dep_type_enum == apt_pkg.Dependency.TYPE_PREDEPENDS) and \
+                    not otherdep.parent_pkg.id in self.deps:
+                return True
+        if self.installed(pkg):
+            for provided in pkg.current_ver.provides_list:
+                try:
+                    provides_pkg = self.cache[provided[0]]
+                    if not self.installed(provides_pkg):
+                        if self.has_revdepends_loop(provides_pkg):
+                            return True
+                except KeyError as e:
+                    self.verboseprint("Package not found:", str(e).strip('"'))
+
+        return False
 
     def is_available(self, pkg):
-        return pkg.has_provides or self.rev_c.installed(pkg)
+        return pkg.has_provides or self.installed(pkg)
 
     def dependency_version(self, pkg):
-        if self.rev_c.installed(pkg):
+        if self.installed(pkg):
             if pkg.id in self.deps:
                 return
             self.deps.add(pkg.id)
@@ -86,13 +107,13 @@ class CirclelessRevdependsCounter:
                     pkg.current_ver.depends_list.get("Depends", []):
                 for otherdep in or_group:
                     if self.is_available(otherdep.target_pkg) and \
-                                    self.rev_c.count_pkg_revdepends_except(otherdep.target_pkg, 1, self.deps) == 0:
+                                    not self.has_revdepends_loop(otherdep.target_pkg):
                         self.dependencies(otherdep.target_pkg)
 
     def dependency_provides(self, pkg):
         if pkg.has_provides:
             for provider in pkg.provides_list:
-                if self.rev_c.installed(provider[2].parent_pkg) and\
+                if self.installed(provider[2].parent_pkg) and\
                         provider[2].parent_pkg.current_ver.id == provider[2].id:
                     self.dependency_version(provider[2].parent_pkg)
 
@@ -100,30 +121,29 @@ class CirclelessRevdependsCounter:
         self.dependency_version(pkg)
         self.dependency_provides(pkg)
 
-    def count_pkg_revrecommends(self, pkg, maxcount):
-            global type_recommends
 
-            self.rev_c.verboseprint("Revrecomms:", pkg.get_fullname(True))
-            rev_recommends = 0
-            pre_rev_recommends = list()
-            for otherdep in pkg.rev_depends_list:
-                if self.rev_c.installed(otherdep.parent_pkg) and \
-                        otherdep.parent_pkg.current_ver.id == otherdep.parent_ver.id and \
-                        otherdep.dep_type_enum == type_recommends:
-                    pre_rev_recommends.append(otherdep.parent_pkg)
+    def is_peak(self, pkg):
+        if self.has_revdeps_without_provides(pkg):
+            return False
+        self.provided_revdeps = list()
+        self.collect_provided_revdeps(pkg)
+        self.revrecommends = list()
+        self.collect_revrecommends(pkg)
 
-            if len(pre_rev_recommends) == 0:
-                return rev_recommends
+        if len(self.provided_revdeps) == 0 and len(self.revrecommends) == 0:
+            return True
 
-            self.rev_c.set_verboseprint(False)
-            self.deps = set()
-            self.dependencies(pkg)
-            self.rev_c.set_verboseprint(self.rev_c.verbose)
+        self.deps = set()
+        self.dependencies(pkg)
 
-            for revd_pkg in pre_rev_recommends:
-                if not revd_pkg.id in self.deps:
-                    rev_recommends += 1
-                    if rev_recommends >= maxcount:
-                        self.rev_c.verboseprint("Found revrecomm:", revd_pkg.get_fullname(True))
-                        return rev_recommends
-            return rev_recommends
+        for prov_revd_pkg in self.provided_revdeps:
+            if not prov_revd_pkg.id in self.deps:
+                self.verboseprint("Found provided reverse depend:", prov_revd_pkg.get_fullname(True))
+                return False
+
+        for revr_pkg in self.revrecommends:
+            if not revr_pkg.id in self.deps:
+                self.verboseprint("Found reverse recommend:", revr_pkg.get_fullname(True))
+                return False
+
+        return True
